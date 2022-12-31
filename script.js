3734024a01882041639031f74b3e64e24838c649
vishnugamini <gamini.vishnu@gmail.com>
2022-12-31 12:08:51 +0530

    modified js file

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 correct number';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+
+document.querySelector('.guess').value = 20;
+console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const num = Number(document.querySelector('.guess').value);
+  if (!num) {
+    document.querySelector('.message').textContent = '🚫 no number!';
+  }
+});
